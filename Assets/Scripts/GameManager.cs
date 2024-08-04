14d63603c3721bf8a4a686c3a8d51aa8e0e8e9c2
0sym1 <TuanNgoc.0358@gmail.com>
2024-08-05 01:22:49 +0700

    cuu toi moi

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
@@ -9,16 +10,19 @@ public class GameManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI scorePlayer1Text;
     [SerializeField] private TextMeshProUGUI scorePlayer2Text;
     [SerializeField] private GameObject ball;
+    [SerializeField] private TextMeshProUGUI goalsText;
+    [SerializeField] private GameObject message;
+    private int goals;
     private int scorePlayer1;
     private int scorePlayer2;
     // Start is called before the first frame update
     void Start()
     {
+        goalsText.text = "";
         scorePlayer1 = 0;
         scorePlayer2 = 0;
     }
     public void addScore(string name){
-        Debug.Log("ok");
         if(name == "Goal1") scorePlayer2++;
         else scorePlayer1++;
 
@@ -34,5 +38,22 @@ public class GameManager : MonoBehaviour
     }
     public void resumeGame(){
         Time.timeScale = 1;
+    }
+    public void playGame(){
+        if(checkInput()) SceneManager.LoadScene(1);
+        else message.SetActive(true);
+    }
+    private bool checkInput(){
+        Debug.Log(goalsText.text + "i");
+        string text = goalsText.text.Trim();
+        Debug.Log(text + "ok");
+        if(int.TryParse(text, out goals)){
+            Debug.Log("ok");
+            return true;
+        }
+        else return false;
+    }
+    public void hiddenMessage(){
+        message.SetActive(false);
     }
 }
